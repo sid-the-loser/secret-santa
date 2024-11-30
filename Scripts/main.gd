37a33505f3b547c53e49a43c3d3b6fb58f62555f
Sidharth Suresh <sidharthsuresh07@gmail.com>
2024-11-30 14:38:26 -0500

    added some features to scripts

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,17 +3,41 @@ extends Control
 @onready var to_label = $"TabContainer/Draw Names/ToLabel"
 @onready var from_line_edit = $"TabContainer/Draw Names/FromLineEdit"
 @onready var list_of_names = $"TabContainer/List of Names"
+@onready var reset_button_name = $"TabContainer/Draw Names/ResetButton".get_text()
 
 var from_names = []
 var from_to_dict = {}
 
 # TODO: work on making everything working in harmony with eachother!
 
-func reset():
+func clear_ouput():
+	to_label.add_theme_color_override("font_color", Color.WHITE)
 	to_label.set_text("")
 	from_line_edit.set_text("")
+
+
+func reset_and_regenerate():
+	clear_ouput()
 	
 	from_to_dict = {}
+
+
+func show_name():
+	var _name = from_line_edit.get_text()
+	
+	if len(from_to_dict) == 0:
+		to_label.add_theme_color_override("font_color", Color.RED)
+		to_label.set_text("Generate the random draws by pressing: "+
+			reset_button_name)
+	elif len(_name) == 0:
+		to_label.add_theme_color_override("font_color", Color.RED)
+		to_label.set_text("Type something in the text-box above!")
+	elif _name in from_to_dict:
+		to_label.add_theme_color_override("font_color", Color.WHITE)
+		to_label.set_text(from_to_dict[_name])
+	else:
+		to_label.add_theme_color_override("font_color", Color.RED)
+		to_label.set_text("Please check if your name is in the list.")
 
 
 func _on_tutorial_button_button_down():
@@ -25,4 +49,12 @@ func _on_support_button_button_down():
 
 
 func _on_reset_button_button_down():
-	pass # Replace with function body.
+	reset_and_regenerate()
+
+
+func _on_clear_button_button_down():
+	clear_ouput()
+
+
+func _on_show_button_button_down():
+	show_name()
