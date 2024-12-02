9cec3cf4465bba604220f76860857edc90e17b5e
Sidharth Suresh <sidharthsuresh07@gmail.com>
2024-12-01 23:30:33 -0500

    fixed more bugs!

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -69,12 +69,14 @@ func reset_and_regenerate():
 					available_names.remove_at(available_names.find(i))
 				else:
 					random_assigning.insert(randi_range(0, len(random_assigning)-1), i)
-					from_to_dict[names[i]] = names[random_assigning[-1]]
 					break
 			
 			random_assigning.append(available_names[randi_range(0, len(available_names)-1)])
 
-			from_to_dict[names[i]] = names[random_assigning[-1]]
+		for i in range(len_names):
+			from_to_dict[names[i]] = names[random_assigning[i]]
+			
+		print(from_to_dict)
 
 
 		to_label.add_theme_color_override("font_color", Color.GREEN)
